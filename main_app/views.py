6dee10d07b283cef50416b67d0fe2ffc5b3b8529
russellasagna <laganzon.r2@yahoo.com>
2022-08-12 21:16:38 -0400

    Add part 1

---
+++ main_app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  # success_url = '/finches/'
+
+  # def get_success_url(self):
+  #   return f'/finches/{self.object.id}'
